fb2302588d1c307df32a9f359964cdf35220eb6c
Huahai Yang <huahai.yang@gmail.com>
2023-01-15 20:57:59 -0800

    release java 8

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Change Log
 
+## 0.4.5
+### Fixed
+- release java 8
+
 ## 0.4.4
 ### Fixed
 - `getSuggestion` return nil when there's no suggestion. (thx @den1k)
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(def version "0.4.4")
+(def version "0.4.5")
 
 (defproject org.clojars.huahaiy/symspell-clj version
   :description "SymSpell spell checker in Clojure"
@@ -6,6 +6,7 @@
   :dependencies [[org.clojure/clojure "1.11.1" :scope "provided"]]
   :scm {:name "git" :url "https://github.com/juji-io/symspell-clj"}
   :java-source-paths ["src/java"]
+  :javac-options ["--release" "8"]
   :deploy-repositories [["clojars" {:url           "https://repo.clojars.org"
                                     :username      :env/clojars_username
                                     :password      :env/clojars_password
